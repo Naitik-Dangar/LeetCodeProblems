6e5b748e776fd0a0bc2630ae713a3f250bab0450
Naitik Dangar <naitikdangar0@gmail.com>
2026-04-17 22:51:28 +0000

    adding test cases

--- TwoSum.py
+++ TwoSum.py
@@ -17,3 +17,21 @@ class Solution:
         return[]
 
 # this is a test
+
+def testCase(inputList, target):
+    print("Input:\nnums = ")
+    print(inputList)
+    print("target = ")
+    print(target)
+    
+    testSol = Solution()
+    outputList = testSol.twoSum(inputList, target)
+    print("Output: ")
+    print(outputList)
+
+print("-----------------testCase1-----------------")
+testCase([2,7,11,15], 9)
+print("-----------------testCase2-----------------")
+testCase([3,2,4], 6)
+print("-----------------testCase3-----------------")
+testCase([3,3], 6)
